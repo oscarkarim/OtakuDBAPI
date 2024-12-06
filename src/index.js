29ca57732550915e056fe478c1c726f115446b54
oscarkarim <oscar.karim@hotmail.com>
2024-12-06 09:26:51 -0600

    modificacion swagger

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ const swaggerOptions = {
       version: '1.0.0',
       description: readmeContent, // Inject the converted README HTML here
     },
-    servers: [{ url: `http://localhost:${port}` }],
+    servers: [{ url: `${domain}` }],
     components: {
       schemas: {
         Anime: {
