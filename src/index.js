5bad2df91571af2a8db9d3c8a41999090b1f473b
oscarkarim <oscar.karim@hotmail.com>
2024-12-07 20:54:21 -0600

    testing servers url change

--- src/index.js
+++ src/index.js
@@ -41,7 +41,8 @@ const swaggerOptions = {
       version: '1.0.0',
       description: readmeContent, // Inject the converted README HTML here
     },
-    servers: [{ url: `${domain}` }],
+    servers: [{ 
+    }],
     components: {
       schemas: {
         Anime: {
